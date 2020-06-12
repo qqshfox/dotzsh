a891e836aac36970865bef65f64519c0276bd3e7
Hanfei Shen <hanfei.shen@alo7.com>
2020-06-12 10:19:39 +0800

    add more alias

--- configs/k8s.zsh
+++ configs/k8s.zsh
@@ -1,4 +1,7 @@
+alias k=kubectl
 alias kbjs="kubectl --context aws-cn-north-1.dev.k8s.local"
 alias kzhy="kubectl --context aws-cn-northwest-1.dev.k8s.local"
-alias kprod="kubectl --context prod-k8s-1"
+alias kprod="kubectl --context aws-cn-north-1.prod-1.k8s.local"
+alias kpbjs="kubectl --context aws-cn-north-1.prod-1.k8s.local"
+alias kpzhy="kubectl --context aws-cn-northwest-1.prod.k8s.local"
 export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
